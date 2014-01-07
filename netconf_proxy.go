b9617933a5c9e14913c9fd2ba8767639f14cc74b
Allan Feid <afeid@shutterstock.com>
2014-01-07 10:38:17 -0500

    initial work, garbage commit

--- netconf_proxy.go
+++ netconf_proxy.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/crazed/ncclient-go"
-	"io"
+	"github.com/Juniper/go-netconf/netconf"
 	"log"
 	"net/http"
 )
@@ -19,24 +17,31 @@ type NetconfRequest struct {
 }
 
 type NetconfResult struct {
-	success bool
-	output  io.Reader
-	client  *ncclient.Ncclient
+	success  bool
+	output   string
+	hostname string
 }
 
-func NetconfWorker(id int, request string, jobs <-chan *ncclient.Ncclient, results chan<- *NetconfResult) {
-	for client := range jobs {
+// This is necessary since netconf.Session has no way to retrieve
+// the hostname/ip that is associated with the session.
+type NetconfWrapper struct {
+	Hostname string
+	Client   *netconf.Session
+}
+
+func NetconfWorker(id int, request string, jobs <-chan *NetconfWrapper, results chan<- *NetconfResult) {
+	for w := range jobs {
 		result := new(NetconfResult)
-		result.client = client
-		if err := client.Connect(); err != nil {
+		result.hostname = w.Hostname
+		if reply, err := w.Client.Exec(request); err != nil {
+			fmt.Println(reply)
 			result.success = false
-			result.output = bytes.NewBufferString(err.Error())
+			result.output = reply.RawReply
 			results <- result
 		} else {
-			defer client.Close()
-			client.SendHello()
-			result.output = client.Write(request)
+			fmt.Println(reply)
 			result.success = true
+			result.output = reply.RawReply
 			results <- result
 		}
 	}
@@ -48,15 +53,20 @@ func NetconfHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: logging..
 	fmt.Println(t)
 
-	jobs := make(chan *ncclient.Ncclient, len(t.Hosts))
+	jobs := make(chan *NetconfWrapper, len(t.Hosts))
 	results := make(chan *NetconfResult, len(t.Hosts))
 
 	// Queue up a bunch of work
 	for i, host := range t.Hosts {
 		log.Println("Creating work for", host)
 		go NetconfWorker(i, t.Request, jobs, results)
-		client := ncclient.MakeClient(t.Username, t.Password, host, t.Port)
-		jobs <- &client
+		target := fmt.Sprintf("%s:%d", host, t.Port)
+		client, err := netconf.DialSSH(target, netconf.SSHConfigPassword(t.Username, t.Password))
+		fmt.Println(client)
+		if err != nil {
+			fmt.Println("Failed to connect to:", host)
+		}
+		jobs <- &NetconfWrapper{Client: client, Hostname: host}
 	}
 	close(jobs)
 
@@ -72,9 +82,9 @@ func NetconfHandler(w http.ResponseWriter, r *http.Request) {
 		result := <-results
 
 		// Pull our entire response output into a string
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(result.output)
-		output := buf.String()
+		//buf := new(bytes.Buffer)
+		//buf.ReadFrom(result.output)
+		//output := buf.String()
 
 		// Create a response structure
 		resp := struct {
@@ -82,8 +92,8 @@ func NetconfHandler(w http.ResponseWriter, r *http.Request) {
 			Output   string
 			Success  bool
 		}{}
-		resp.Hostname = result.client.Hostname()
-		resp.Output = output
+		resp.Hostname = result.hostname
+		resp.Output = result.output
 		resp.Success = result.success
 
 		// Flush this line
